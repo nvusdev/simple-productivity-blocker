f912042f7686fd5e22829ca3323a3e561d0595b3
nevicnevic <neptaliaaron@gmail.com>
2026-05-06 23:28:49 -0500

    Fix GitHub Pages asset pathing with conditional basePath

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig = {
   output: 'export',
+  basePath: isGithubActions ? '/simple-productivity-blocker' : '',
+  assetPrefix: isGithubActions ? '/simple-productivity-blocker/' : '',
   images: {
     unoptimized: true,
   },
